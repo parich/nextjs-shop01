29be9cbeeb667947e3d3aa891d02c6e0705c43c8
parich <parich.suriya@gmail.com>
2022-07-31 20:13:35 +0700

    Create Layout Component

---
+++ components/Layout.js
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
+
+export default function Layout({ children, title, description }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - NextShop' : 'NextShop'}</title>
+        {description && <meta content={description} name="description"></meta>}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between ">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/">
+              <a className="text-lg font-bold">NextShop</a>
+            </Link>
+            <div>
+              <Link href="/cart">
+                <a className="p-2">Cart</a>
+              </Link>
+              <Link href="/cart">
+                <a className="p-2">Logout</a>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright © 2022 NextShop</p>
+        </footer>
+      </div>
+    </>
+  );
+}
